c560d9fa23ce5bfabeda1b405ecb603b1693e4a7
App Academy Student <student@appacademy.io>
2016-07-20 18:02:04 -0700

    game in progress

---
+++ lib/game.rb
@@ -0,0 +1,23 @@
+class Game
+
+  def initialize(players)
+    @players = players
+    @active_players = players
+    @minimum_bet = 0
+    @current_player = players.first
+  end
+
+  def play_hand
+  end
+
+  def change_player
+  end
+
+  def winner
+  end
+
+  def deal_hands
+    # players.each do | player |
+  end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,37 @@
+describe Game do
+
+  describe "#initialize" do
+    it "initalizes with players"
+  end
+
+  describe "#play_hand" do
+    it "generates a new deck"
+  end
+
+  describe "#change_player" do
+    it "advances to the next player"
+  end
+
+  describe "#winner" do
+    it "determines the winner"
+    it "awards winnings"
+  end
+
+  describe "#deal_hands" do
+    it "generates a new hand for each player"
+  end
+
+  describe "#place_bets" do
+    it "collects player bets"
+    it "increases the pot"
+  end
+
+  describe "#give_card" do
+    it "draws a new card from the deck"
+    it "passes the new card to the player"
+  end
+
+  describe "#fold_player" do
+    it "removes the player from the players"
+
+  describe "#valid_bet?"
